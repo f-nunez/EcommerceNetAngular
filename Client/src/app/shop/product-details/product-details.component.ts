5a21afc4b746e59b328d05f472ddbad54f5de5af
Francisco Nuñez <fonfonferi@gmail.com>
2022-08-14 21:31:54 -0700

    Adding section header component #5

--- Client/src/app/shop/product-details/product-details.component.ts
+++ Client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -11,7 +12,7 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private breadcrumbService: BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -19,7 +20,10 @@ export class ProductDetailsComponent implements OnInit {
 
   loadProduct() {
     this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe({
-      next: (response) => this.product = response,
+      next: (response) => {
+        this.product = response;
+        this.breadcrumbService.set('@productDetails', this.product.name);
+      },
       error: (error) => console.log(error)
     });
   }
