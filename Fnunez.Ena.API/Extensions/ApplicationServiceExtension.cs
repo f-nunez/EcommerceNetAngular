38b5be1ed224b5da2988210135ecf23e79a2e558
Francisco Nuñez <fonfonferi@gmail.com>
2022-09-27 17:57:03 -0700

    Add BasketRepository to dependency container #6

--- Fnunez.Ena.API/Extensions/ApplicationServiceExtension.cs
+++ Fnunez.Ena.API/Extensions/ApplicationServiceExtension.cs
@@ -11,6 +11,7 @@ public static class ApplicationServiceExtension
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+        services.AddScoped<IBasketRepository, BasketRepository>();
         services.AddScoped<IProductRepository, ProductRepository>();
         services.Configure<ApiBehaviorOptions>(options =>
         {
