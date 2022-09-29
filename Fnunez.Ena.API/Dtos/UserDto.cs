43171aa200533acbbc8b567ff289b351d61de838
Francisco Nuñez <fonfonferi@gmail.com>
2022-09-28 21:46:50 -0700

    UserDto #8

---
+++ Fnunez.Ena.API/Dtos/UserDto.cs
@@ -0,0 +1,8 @@
+namespace Fnunez.Ena.API.Dtos;
+
+public class UserDto
+{
+    public string Email { get; set; }
+    public string DisplayName { get; set; }
+    public string Token { get; set; }
+}
