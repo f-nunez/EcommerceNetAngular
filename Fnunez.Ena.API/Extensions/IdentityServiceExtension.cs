b80f312e7d464f11edc6b0955ab0793adf2b3c40
Francisco Nuñez <fonfonferi@gmail.com>
2022-09-29 18:35:57 -0700

    Add JsonWebToken into IdentityServiceExtension #8

--- Fnunez.Ena.API/Extensions/IdentityServiceExtension.cs
+++ Fnunez.Ena.API/Extensions/IdentityServiceExtension.cs
@@ -1,18 +1,34 @@
+using System.Text;
 using Fnunez.Ena.Core.Entities.Identity;
 using Fnunez.Ena.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Fnunez.Ena.API.Extensions;
 
 public static class IdentityServiceExtension
 {
-    public static IServiceCollection AddIdentityServices(this IServiceCollection services)
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services,
+    IConfiguration configuration)
     {
         IdentityBuilder builder = services.AddIdentityCore<AppUser>();
         builder = new IdentityBuilder(builder.UserType, builder.Services);
         builder.AddEntityFrameworkStores<AppIdentityDbContext>();
         builder.AddSignInManager<SignInManager<AppUser>>();
-        services.AddAuthentication();
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(configuration["Token:Key"])),
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["Token:Issuer"],
+                    ValidateAudience = false
+                };
+            });
 
         return services;
     }
