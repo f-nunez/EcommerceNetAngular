1a3d46ff5430efc43c031a30c7311b6f02546d3f
Francisco Nuñez <fonfonferi@gmail.com>
2022-09-29 18:38:57 -0700

    Update action methods on AccountController #8

--- Fnunez.Ena.API/Controllers/AccountController.cs
+++ Fnunez.Ena.API/Controllers/AccountController.cs
@@ -1,7 +1,12 @@
 using System.Net;
+using System.Security.Claims;
+using AutoMapper;
 using Fnunez.Ena.API.Dtos;
 using Fnunez.Ena.API.Errors;
+using Fnunez.Ena.API.Extensions;
 using Fnunez.Ena.Core.Entities.Identity;
+using Fnunez.Ena.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,11 +16,40 @@ public class AccountController : BaseApiController
 {
     private readonly SignInManager<AppUser> _signInManager;
     private readonly UserManager<AppUser> _userManager;
+    private readonly ITokenService _tokenService;
+    private readonly IMapper _mapper;
 
-    public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+    public AccountController(
+        SignInManager<AppUser> signInManager,
+        UserManager<AppUser> userManager,
+        ITokenService tokenService,
+        IMapper mapper)
     {
         _signInManager = signInManager;
         _userManager = userManager;
+        _tokenService = tokenService;
+        _mapper = mapper;
+    }
+
+    [Authorize]
+    [HttpGet("getcurrentuser")]
+    public async Task<ActionResult<UserDto>> GetCurrentUser()
+    {
+        AppUser user = await _userManager
+            .FindUserByEmailFromClaimsPrincipalAsync(HttpContext.User);
+
+        return new UserDto
+        {
+            DisplayName = user.DisplayName,
+            Email = user.Email,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
+    [HttpGet("emailexists")]
+    public async Task<ActionResult<bool>> EmailExists([FromQuery] string email)
+    {
+        return await _userManager.FindByEmailAsync(email) != null;
     }
 
     [HttpPost("login")]
@@ -36,7 +70,7 @@ public class AccountController : BaseApiController
         {
             DisplayName = user.DisplayName,
             Email = user.Email,
-            Token = "This will have a token"
+            Token = _tokenService.CreateToken(user)
         };
     }
 
@@ -59,7 +93,34 @@ public class AccountController : BaseApiController
         {
             DisplayName = user.DisplayName,
             Email = user.Email,
-            Token = "This will have a token"
+            Token = _tokenService.CreateToken(user)
         };
+    }
+
+    [Authorize]
+    [HttpGet("address")]
+    public async Task<ActionResult<AddressDto>> GetUserAddress()
+    {
+        AppUser user = await _userManager
+            .FindUserByEmailFromClaimsPrincipalWithAddressAsync(HttpContext.User);
+
+        return _mapper.Map<Address, AddressDto>(user.Address);
+    }
+
+    [Authorize]
+    [HttpPut("address")]
+    public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto addressDto)
+    {
+        AppUser user = await _userManager
+            .FindUserByEmailFromClaimsPrincipalWithAddressAsync(HttpContext.User);
+
+        user.Address = _mapper.Map<AddressDto, Address>(addressDto);
+
+        var results = await _userManager.UpdateAsync(user);
+
+        if (!results.Succeeded)
+            return BadRequest("Wrongs updating User's info");
+
+        return Ok(_mapper.Map<Address, AddressDto>(user.Address));
     }
 }
