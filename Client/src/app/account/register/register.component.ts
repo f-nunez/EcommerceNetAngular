3dfe986dd0dcac9f13cfdbf5f9609b9f45940d5a
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-03 00:29:37 -0700

    Add register form #10

--- Client/src/app/account/register/register.component.ts
+++ Client/src/app/account/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  errors: string[];
 
-  constructor() { }
+  constructor(private accountService: AccountService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      displayName: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.pattern('[\\w-]+@([\\w-]+\\.)+[\\w-]+')]],
+      password: [null, [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => this.router.navigateByUrl('/shop'),
+      error: (error) => {
+        let errorProperties = Object.keys(error.errors);
+        let errorArray = [];
+        for (let errorProperty of errorProperties) {
+          errorArray.push(error.errors[errorProperty]);
+        }
+        this.errors = errorArray;
+      }
+    }
+    );
   }
 
 }
