5957ec793912c3af512b1afaeec154c888adf536
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-06 11:49:07 -0700

    Add Order models to database context #11

--- Fnunez.Ena.Infrastructure/Data/StoreDbContext.cs
+++ Fnunez.Ena.Infrastructure/Data/StoreDbContext.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using Fnunez.Ena.Core.Entities;
+using Fnunez.Ena.Core.Entities.OrderAggregate;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fnunez.Ena.Infrastructure.Data;
@@ -9,6 +10,10 @@ public class StoreDbContext : DbContext
     public DbSet<ProductBrand> ProductBrands { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<ProductType> ProductTypes { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
+    
 
     public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
     {
