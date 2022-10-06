59b73f863955b4d652b047cab7ee9c2165f40836
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-06 11:51:02 -0700

    Migration to add Orders #11

--- Fnunez.Ena.Infrastructure/Data/Migrations/20221006060337_OrderEntity.Designer.cs
+++ Fnunez.Ena.Infrastructure/Data/Migrations/20221006060337_OrderEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Fnunez.Ena.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fnunez.Ena.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221006060337_OrderEntity")]
+    partial class OrderEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.9");
