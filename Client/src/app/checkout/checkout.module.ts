d95ee8ba25acbdc7bd485426e8bf4842a7da0e20
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-11 00:10:19 -0700

    CheckoutSuccess component #12

--- Client/src/app/checkout/checkout.module.ts
+++ Client/src/app/checkout/checkout.module.ts
@@ -7,6 +7,7 @@ import { CheckoutAddressComponent } from './checkout-address/checkout-address.co
 import { CheckoutDeliveryComponent } from './checkout-delivery/checkout-delivery.component';
 import { CheckoutReviewComponent } from './checkout-review/checkout-review.component';
 import { CheckoutPaymentComponent } from './checkout-payment/checkout-payment.component';
+import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
 
 
 
@@ -17,6 +18,7 @@ import { CheckoutPaymentComponent } from './checkout-payment/checkout-payment.co
     CheckoutDeliveryComponent,
     CheckoutReviewComponent,
     CheckoutPaymentComponent,
+    CheckoutSuccessComponent
   ],
   imports: [
     CommonModule,
