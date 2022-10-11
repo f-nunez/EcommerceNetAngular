af0b33c975c72b2c5d7c7f6cb381510ea5e107d4
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-11 00:06:52 -0700

    Change ShippingToAddress to ShipToAddress #12

--- Fnunez.Ena.API/Dtos/OrderDto.cs
+++ Fnunez.Ena.API/Dtos/OrderDto.cs
@@ -4,5 +4,5 @@ public class OrderDto
 {
     public string BasketId { get; set; }
     public int DeliveryMethodId { get; set; }
-    public AddressDto ShippingToAddress { get; set; }
+    public AddressDto ShipToAddress { get; set; }
 }
