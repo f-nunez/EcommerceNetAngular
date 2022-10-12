65814ebc7d53960766bd257df21ccdd5c240b863
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-12 11:18:28 -0700

    Add class members to save payment data on CustomerBasket #14

--- Fnunez.Ena.Core/Entities/CustomerBasket.cs
+++ Fnunez.Ena.Core/Entities/CustomerBasket.cs
@@ -4,6 +4,9 @@ public class CustomerBasket
 {
     public string Id { get; set; }
     public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    public int? DeliveryMethodId { get; set; }
+    public string ClientSecret { get; set; }
+    public string PaymentIntentId { get; set; }
 
     public CustomerBasket()
     {
