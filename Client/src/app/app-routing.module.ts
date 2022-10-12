1a93faf0e12bbfeef402980c7eb0f9915984ae8c
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-11 19:07:57 -0700

    Add auth guard on Orders route #13

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -19,7 +19,12 @@ const routes: Routes = [
     loadChildren: () => import('./checkout/checkout.module').then(module => module.CheckoutModule),
     data: { breadcrumb: 'Checkout' }
   },
-  { path: 'orders', loadChildren: () => import('./orders/orders.module').then(module => module.OrdersModule), data: { breadcrumb: 'Orders' } },
+  {
+    path: 'orders',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./orders/orders.module').then(module => module.OrdersModule),
+    data: { breadcrumb: 'Orders' }
+  },
   { path: 'account', loadChildren: () => import('./account/account.module').then(module => module.AccountModule), data: { breadcrumb: { skip: true } } },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
