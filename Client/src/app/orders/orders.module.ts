d5493ef6370c6a931d411cec28ae52e2c6860129
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-11 19:59:26 -0700

    Update values for reusable shared components #13

--- Client/src/app/orders/orders.module.ts
+++ Client/src/app/orders/orders.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
 import { OrdersRoutingModule } from './orders-routing.module';
+import { SharedModule } from '../shared/shared.module';
 import { OrderDetailsComponent } from './order-details/order-details.component';
 
 
 
 @NgModule({
   declarations: [
-    OrdersComponent,
-    OrderDetailsComponent
+    OrdersComponent, OrderDetailsComponent
   ],
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    SharedModule
   ]
 })
 export class OrdersModule { }
