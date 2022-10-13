619ebb754400b7eea6669e44b54da9e84c557cfe
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-12 17:13:24 -0700

    Refactor CheckoutPayment component #14

--- Client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ Client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -26,6 +26,7 @@ export class CheckoutPaymentComponent implements OnInit, AfterViewInit, OnDestro
   cardCvc: any;
   cardErrors: any;
   cardHandler = this.onChange.bind(this);
+  loading: boolean = false;
 
   constructor(
     private basketService: BasketService,
@@ -67,31 +68,41 @@ export class CheckoutPaymentComponent implements OnInit, AfterViewInit, OnDestro
     }
   }
 
-  submitOrder() {
+  async submitOrder() {
+    this.loading = true;
     const basket = this.basketService.getCurrentBasketValue();
-    const orderToCreate = this.getOrderToCreate(basket);
-    this.checkoutService.createOrder(orderToCreate).subscribe((order: IOrder) => {
-      this.toastr.success('Order created successfully');
-      this.stripe.confirmCardPayment(basket.clientSecret, {
-        payment_method: {
-          card: this.cardNumber,
-          billing_details: {
-            name: this.checkoutForm.get('paymentForm').get('nameOnCard').value
-          }
-        }
-      }).then(result => {
-        console.log(result);
-        if (result.paymentIntent) {
-          this.basketService.deleteBasketOrdered(basket.id);
-          const navigationExtras: NavigationExtras = { state: order };
-          this.router.navigate(['checkout/success'], navigationExtras);
-        } else {
-          this.toastr.error('Payment error');
-        }
-      });
-    }, error => {
+    const createdOrder = await this.createOrder(basket);
+    const paymentResult = await this.confirmPaymentWithStripe(basket);
+
+    try {
+      if (paymentResult.paymentIntent) {
+        this.basketService.deleteBasketOrdered(basket.id);
+        const navigationExtras: NavigationExtras = { state: createdOrder };
+        this.router.navigate(['checkout/success'], navigationExtras);
+      } else {
+        this.toastr.error(paymentResult.error.message);
+      }
+      this.loading = false;
+    } catch (error) {
       console.log(error);
+      this.loading = false;
+    }
+  }
+
+  private async confirmPaymentWithStripe(basket: IBasket) {
+    return this.stripe.confirmCardPayment(basket.clientSecret, {
+      payment_method: {
+        card: this.cardNumber,
+        billing_details: {
+          name: this.checkoutForm.get('paymentForm').get('nameOnCard').value
+        }
+      }
     });
+  }
+
+  private async createOrder(basket: IBasket) {
+    const orderToCreate = this.getOrderToCreate(basket);
+    return this.checkoutService.createOrder(orderToCreate).toPromise();
   }
 
   private getOrderToCreate(basket: IBasket) {
