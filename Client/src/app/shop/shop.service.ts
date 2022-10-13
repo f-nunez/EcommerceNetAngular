6b957775edb622037c76895f0070b4a8ab34ea7a
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-12 22:28:53 -0700

    Get and set shop params as local #15

--- Client/src/app/shop/shop.service.ts
+++ Client/src/app/shop/shop.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { map, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { IBrand } from '../shared/models/brand';
-import { IPagination } from '../shared/models/pagination';
+import { IPagination, Pagination } from '../shared/models/pagination';
 import { IProduct } from '../shared/models/product';
 import { IProductType } from '../shared/models/productType';
 import { ShopParams } from '../shared/models/shopParams';
@@ -16,6 +16,8 @@ export class ShopService {
   brands: IBrand[] = [];
   products: IProduct[] = [];
   productTypes: IProductType[] = [];
+  pagination: Pagination = new Pagination();
+  shopParams: ShopParams = new ShopParams();
 
   constructor(private http: HttpClient) { }
 
@@ -42,29 +44,29 @@ export class ShopService {
     return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
-  getProducts(shopParams: ShopParams) {
+  getProducts() {
     let params = new HttpParams();
 
-    if (shopParams.brandId !== 0) {
-      params = params.append('brandId', shopParams.brandId.toString());
+    if (this.shopParams.brandId !== 0) {
+      params = params.append('brandId', this.shopParams.brandId.toString());
     }
 
-    if (shopParams.productTypeId !== 0) {
-      params = params.append('typeId', shopParams.productTypeId.toString());
+    if (this.shopParams.productTypeId !== 0) {
+      params = params.append('typeId', this.shopParams.productTypeId.toString());
     }
 
-    if (shopParams.search) {
-      params = params.append('search', shopParams.search);
+    if (this.shopParams.search) {
+      params = params.append('search', this.shopParams.search);
     }
 
-    params = params.append('sort', shopParams.sort);
-    params = params.append('pageIndex', shopParams.pageNumber.toString());
-    params = params.append('pageSize', shopParams.pageSize.toString());
+    params = params.append('sort', this.shopParams.sort);
+    params = params.append('pageIndex', this.shopParams.pageNumber.toString());
+    params = params.append('pageSize', this.shopParams.pageSize.toString());
 
     return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
       .pipe(
         map(response => {
-          this.products = response.body.data;
+          this.products = [...this.products, ...response.body.data];
           return response.body;
         })
       );
@@ -81,5 +83,13 @@ export class ShopService {
         return response;
       })
     );
+  }
+
+  getShopParams() {
+    return this.shopParams;
+  }
+
+  setShopParams(shopParams: ShopParams) {
+    this.shopParams = shopParams;
   }
 }
