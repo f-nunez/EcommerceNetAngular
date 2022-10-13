943965a8dc038c33ccf669fdca6a41a9e7bdc71e
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-13 14:35:56 -0700

    Migrations to create Identity database

--- Fnunez.Ena.Infrastructure/Identity/Migrations/20221013212325_Initial.Designer.cs
+++ Fnunez.Ena.Infrastructure/Identity/Migrations/20221013212325_Initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Fnunez.Ena.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Fnunez.Ena.Infrastructure.Identity.Migrations
 {
     [DbContext(typeof(AppIdentityDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221013212325_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
