925086ec33cc780d69f61647f2f49f0947933ecb
Francisco Nuñez <fonfonferi@gmail.com>
2023-04-28 08:18:58 -0700

    Refactoring paging-footer component

--- Client/src/app/shared/components/paging-footer/paging-footer.component.ts
+++ Client/src/app/shared/components/paging-footer/paging-footer.component.ts
@@ -1,23 +1,19 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-paging-footer',
   templateUrl: './paging-footer.component.html',
   styleUrls: ['./paging-footer.component.scss']
 })
-export class PagingFooterComponent implements OnInit {
-  @Input() pageSize: number;
-  @Input() pageNumber: number;
-  @Input() totalCount: number;
+export class PagingFooterComponent {
+  @Input() pageSize?: number;
+  @Input() pageNumber?: number;
+  @Input() totalCount?: number;
   @Output() pageChanged = new EventEmitter<number>();
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
   onPagingFooterChange(event: any) {
     this.pageChanged.emit(event.page);
   }
-
 }
