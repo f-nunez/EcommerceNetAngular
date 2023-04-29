dbcf2119ace0e0ef7bc4f1f88a6a54069d8e6bd3
Francisco Nuñez <fonfonferi@gmail.com>
2023-04-29 10:37:19 -0700

    Refactor server-error component

--- Client/src/app/core/server-error/server-error.component.ts
+++ Client/src/app/core/server-error/server-error.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,15 +6,11 @@ import { Router } from '@angular/router';
   templateUrl: './server-error.component.html',
   styleUrls: ['./server-error.component.scss']
 })
-export class ServerErrorComponent implements OnInit {
+export class ServerErrorComponent {
   error: any;
 
   constructor(private router: Router) {
     const navigation = this.router.getCurrentNavigation();
-    this.error = navigation?.extras?.state?.error;
+    this.error = navigation?.extras?.state?.['error'];
   }
-
-  ngOnInit(): void {
-  }
-
 }
