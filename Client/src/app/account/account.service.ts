24c33929815de4ec4980b55cde602aebcda52605
Francisco Nuñez <fonfonferi@gmail.com>
2023-04-30 08:17:02 -0700

    Refactor account module

--- Client/src/app/account/account.service.ts
+++ Client/src/app/account/account.service.ts
@@ -1,17 +1,17 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, map, of, ReplaySubject } from 'rxjs';
+import { map, of, ReplaySubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { IAddress } from '../shared/models/address';
-import { IUser } from '../shared/models/user';
+import { Address } from '../shared/models/address';
+import { User } from '../shared/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   baseUrl = environment.apiUrl;
-  private currentUserSource = new ReplaySubject<IUser>(1);
+  private currentUserSource = new ReplaySubject<User | null>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient, private router: Router) { }
@@ -21,10 +21,10 @@ export class AccountService {
   }
 
   getUserAddress() {
-    return this.http.get<IAddress>(this.baseUrl + 'account/address');
+    return this.http.get<Address>(this.baseUrl + 'account/address');
   }
 
-  loadCurrentUser(token: string) {
+  loadCurrentUser(token: string | null) {
     if (token === null) {
       this.currentUserSource.next(null);
       return of(null);
@@ -33,19 +33,22 @@ export class AccountService {
     let headers = new HttpHeaders();
     headers = headers.set('Authorization', `Bearer ${token}`);
 
-    return this.http.get(this.baseUrl + 'account/getcurrentuser', { headers }).pipe(
-      map((user: IUser) => {
+    return this.http.get<User>(this.baseUrl + 'account/getcurrentuser', { headers }).pipe(
+      map((user) => {
         if (user) {
           localStorage.setItem('token', user.token);
           this.currentUserSource.next(user);
+          return user;
+        } else {
+          return null;
         }
       })
     );
   }
 
   login(values: any) {
-    return this.http.post(this.baseUrl + 'account/login', values).pipe(
-      map((user: IUser) => {
+    return this.http.post<User>(this.baseUrl + 'account/login', values).pipe(
+      map((user) => {
         if (user) {
           localStorage.setItem('token', user.token);
           this.currentUserSource.next(user);
@@ -61,8 +64,8 @@ export class AccountService {
   }
 
   register(values: any) {
-    return this.http.post(this.baseUrl + 'account/register', values).pipe(
-      map((user: IUser) => {
+    return this.http.post<User>(this.baseUrl + 'account/register', values).pipe(
+      map((user) => {
         if (user) {
           localStorage.setItem('token', user.token);
           this.currentUserSource.next(user);
@@ -71,7 +74,7 @@ export class AccountService {
     );
   }
 
-  updateUserAddress(address: IAddress) {
-    return this.http.put<IAddress>(this.baseUrl + 'account/address', address);
+  updateUserAddress(address: Address) {
+    return this.http.put<Address>(this.baseUrl + 'account/address', address);
   }
 }
